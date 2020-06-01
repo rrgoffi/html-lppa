0e9c6ba1ff34e78892cf6edf613913807c9f243e
Ricardo Roberto Goffi <rrgoffi@gmail.com>
2020-06-01 10:28:54 -0300

    Se agrego el alta y vista de la entidad producto

---
+++ SolutionLPPA/LPPA.Web.Site/Controllers/ProductoController.cs
@@ -0,0 +1,36 @@
+﻿using LPPA.Web.Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LPPA.Web.Site.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection form)
+        {
+            //
+            var producto = new Producto();
+
+            producto.Codigo = Convert.ToInt32(form["codigo"]);
+            producto.Nombre = form["nombre"];
+            producto.descripcion = form["descripcion"];
+
+            Dominio.Add(producto);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SolutionLPPA/LPPA.Web.Site/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace LPPA.Web.Site.Models
+{
+    public class Producto
+    {
+        public int Codigo { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string descripcion { get; set; }
+    }
+}
